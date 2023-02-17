9df08f301f894b2951e2771c1f0635defecd38b9
chadcarlson <chad.carlson@platform.sh>
2023-02-17 14:47:47 -0500

    Try to update all three.

---
+++ install-poetry.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Upgrade pip
+python3.10 -m pip install --upgrade pip
+
+# Install poetry.
+export PIP_USER=false
+curl -sSL https://install.python-poetry.org | python3 - --version $POETRY_VERSION
+export PATH="/app/.local/bin:$PATH"
+export PIP_USER=true
+
+# Check lockfile and environment.
+poetry env use system
+poetry check
